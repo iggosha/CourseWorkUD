9bdce458d29155d20b77935620bd4c080222e509
iggosha <iegolovkov@gmail.com>
2023-05-12 15:52:38 +0300

    Добавлена вставка данных в таблицы

--- src/main/java/com/course/courseud/InsertsController.java
+++ src/main/java/com/course/courseud/InsertsController.java
@@ -10,19 +10,19 @@ public class InsertsController {
     @FXML
     private Button clearButton;
     @FXML
-    private Button deleteButton;
-    @FXML
     private ComboBox<String> tablesComboBox;
     @FXML
-    private TableView<ObservableList<String>> deletesTable;
+    private TextField insertsTextField;
+    @FXML
+    private TableView<ObservableList<String>> insertsTable;
 
     UtilsController utilsController = new UtilsController();
 
     @FXML
     public void initialize() {
-        clearButton.setOnAction(actionEvent -> utilsController.clearTable(deletesTable));
+        clearButton.setOnAction(actionEvent -> utilsController.clearTable(insertsTable));
         goToMenuButton.setOnAction(actionEvent -> utilsController.openNewWindow(goToMenuButton, "menu.fxml"));
-        deleteButton.setOnAction(actionEvent -> deleteRow());
+        insertsTextField.setOnAction(actionEvent -> insertRow());
         tablesComboBox.setOnAction(actionEvent -> makeTableView());
         fillTablesComboBox();
     }
@@ -32,19 +32,37 @@ public class InsertsController {
     }
 
     private void makeTableView() {
-        deletesTable.getColumns().clear();
+        insertsTable.getColumns().clear();
         String sqlQuery = "SELECT * FROM ";
         // Получаем данные из выбранной таблицы
         sqlQuery = sqlQuery + tablesComboBox.getValue();
-        utilsController.fillTableWithSqlQuery(deletesTable, sqlQuery);
+        utilsController.fillTableWithSqlQuery(insertsTable, sqlQuery);
     }
 
 
     private void insertRow() {
-        String sqlQuery = "INSERT INTO " + tablesComboBox.getValue() + " VALUES(";
+        StringBuilder sqlQuery = new StringBuilder("INSERT INTO " + tablesComboBox.getValue() + "(");
+        for (TableColumn<ObservableList<String>, ?> tc : insertsTable.getColumns()) {
+            sqlQuery.append(tc.getText())
+                    .append(", ");
+        }
+        sqlQuery.deleteCharAt(sqlQuery.length() - 2)
+                .append(") VALUES (");
         String regex = "\\d+";
-        utilsController.updateTableWithSqlQuery(sqlQuery);
+        String[] values = insertsTextField.getText().split(",");
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String value : values) {
+            value = value.trim();
+            if (!value.matches(regex)) {
+                value = "'" + value + "'";
+            }
+            stringBuilder.append(value)
+                    .append(", ");
+        }
+        stringBuilder.deleteCharAt(stringBuilder.length() - 2);
+        sqlQuery.append(stringBuilder)
+                .append(");");
+        utilsController.updateTableWithSqlQuery(String.valueOf(sqlQuery));
         makeTableView();
-
     }
 }
